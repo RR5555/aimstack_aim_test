68f875e030b488ff46dee3fb6b0a98d0d24044ef
RR5555 <renardromain@hotmail.fr>
2026-09-03 17:05:21 +0400

    feat(is_dockerfile_newer): add nanoseconds to time comparison; invert logic from `is_dockerfile_older` to `is dockerfile_newer`

--- fcts.sh
+++ fcts.sh
@@ -12,11 +12,39 @@ function docker_image_exists () {
 	docker image inspect "$1" >/dev/null 2>&1
 }
 
-# is_dockerfile_older <tag_name> <Dockerfile_path> 
-function is_dockerfile_older () {
-	# stat -c '%Y' "$2"
+# is_dockerfile_newer <tag_name> <Dockerfile_path> 
+function is_dockerfile_newer () {
+	# GNU Date: Run date `+%s.%N` to output seconds with nanoseconds (e.g., `1725375612.123456789`)
+	# Date given in:
+	# ```bash
+	# docker image inspect "$1" --format='{{.Metadata.LastTagTime}}'
+	# ```
+	# Is in Golang format not comaptible with bash `date`
+	# It can be turned into Unix epoch:
+	# ```bash
 	# docker image inspect "$1" --format='{{.Metadata.LastTagTime.Unix}}'
-	[[ "$(stat -c '%Y' "$2")" < "$(docker image inspect "$1" --format='{{.Metadata.LastTagTime.Unix}}')" ]]
+	# 1788439277
+	# ```
+	# To get the nanoseconds (format `%s%N`):
+	# ```bash
+	# docker image inspect "$1" --format='{{.Metadata.LastTagTime.UnixNano}}'
+	# 1788439277244226334
+	# ```
+	# To only get the nanoseconds:
+	# ```bash
+	# docker image inspect "$1" --format='{{.Metadata.LastTagTime.Nanosecond}}'
+	# 244226334
+	# ```
+	# Thus to get `%s%N`:
+	# ```bash
+	# docker image inspect "$1" --format='{{.Metadata.LastTagTime.Unix}}.{{.Metadata.LastTagTime.Nanosecond}}'
+	# ```
+	# For simple condition testing, sue:
+	# ```bash
+	# [[ 1 < 2 ]]; echo $?
+	# ```
+
+	[[ "$(stat -c '%y' "$2" | xargs -I {} date --date {} "+%s.%N")" > "$(docker image inspect "$1" --format='{{.Metadata.LastTagTime.Unix}}.{{.Metadata.LastTagTime.Nanosecond}}')" ]]
 }
 
 
